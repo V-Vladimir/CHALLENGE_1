1ea5b5118aeb418cbc682bbd202506857e234614
Vladimir Vdovchenko <vladimir@vdovchenko.com>
2023-02-05 08:29:39 +0200

    first version

---
+++ CHALLENGE_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CHALLENGE_1
+//
+//  Created by Vladimir Vdovchenko on 05.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
