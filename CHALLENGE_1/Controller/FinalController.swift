c9ca0b83cce9b35d93d559fe00aa52cf7a6e779a
Sergey Azimov <74828253+FarbosZ@users.noreply.github.com>
2023-02-06 13:37:29 +0200

    создал новый экран. первая версия final ViewController

---
+++ CHALLENGE_1/Controller/FinalController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  CHALLENGE_1
+//
+//  Created by Vladimir Vdovchenko on 05.02.2023.
+//
+
+import UIKit
+
+class FinalController: UIViewController {
+    var questionsModel = CQuestions()
+    let finalView = FinalView()
+    
+    let playAgainButton: UIButton = {
+        
+        let button = UIButton(type: .system)
+        button.backgroundColor = #colorLiteral(red: 0.2319103479, green: 0.7549440265, blue: 0.2910608053, alpha: 1)
+        button.setTitle("PlAY AGAIN", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "ArialMT", size: 36)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 20
+        button.addTarget(nil, action: #selector(buttonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    override func loadView() {
+        view = finalView
+        // if you'd like to  change titles
+        
+        //finalView.textInformation.text = "you're score is 12 "
+        //finalView.finalResultText.text = "LOSE"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(playAgainButton)
+    setConstraints()
+
+    }
+
+    @objc  func buttonTapped(sender: UIButton) {
+        //code for segue
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            
+            playAgainButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -20),
+            playAgainButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            playAgainButton.widthAnchor.constraint(equalToConstant: 282),
+            playAgainButton.heightAnchor.constraint(equalToConstant: 87),
+        ])
+    }
+}
+
---
+++ CHALLENGE_1/View/FinalView.swift
@@ -0,0 +1,89 @@
+//
+//  RestartView.swift
+//  CHALLENGE_1
+//
+//  Created by Sergey Azimov on 05.02.2023.
+//
+
+import UIKit
+
+class FinalView: UIView {
+
+    let backgroundImage:UIImageView = {
+        let backgroundImage = UIImageView()
+        backgroundImage.image = UIImage(named: "Frame")
+        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+        return backgroundImage
+    }()
+    
+    let titleImage: UIImageView = {
+       let titleImage = UIImageView()
+        titleImage.image = UIImage(named: "Logo")
+        titleImage.translatesAutoresizingMaskIntoConstraints = false
+        return titleImage
+    }()
+    
+    let textInformation: UILabel = {
+       let textLabel = UILabel()
+        textLabel.text = "You losed on {attempt} attempt"
+        textLabel.textColor = .white
+        textLabel.font = UIFont(name: "ArialMT", size: 24)
+        textLabel.textAlignment = .center
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        return textLabel
+    }()
+    
+    let finalResultText: UILabel = {
+       let textLabel = UILabel()
+        textLabel.text = "LOSE"
+        textLabel.textColor = .red
+        textLabel.font = UIFont(name: "ArialMT", size: 50)
+        textLabel.textAlignment = .center
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        return textLabel
+    }()
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addView()
+    setConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addView() {
+        addSubview(backgroundImage)
+        addSubview(titleImage)
+        addSubview(textInformation)
+        addSubview(finalResultText)
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            
+            backgroundImage.topAnchor.constraint(equalTo: topAnchor),
+            backgroundImage.bottomAnchor.constraint(equalTo: bottomAnchor),
+            backgroundImage.leadingAnchor.constraint(equalTo: leadingAnchor),
+            backgroundImage.trailingAnchor.constraint(equalTo: trailingAnchor),
+            
+            titleImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 19),
+            titleImage.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleImage.widthAnchor.constraint(equalToConstant: 201),
+            titleImage.heightAnchor.constraint(equalToConstant: 201),
+            
+            textInformation.topAnchor.constraint(equalTo: titleImage.bottomAnchor, constant: 5),
+            textInformation.centerXAnchor.constraint(equalTo: centerXAnchor),
+            textInformation.widthAnchor.constraint(equalToConstant: 345),
+            textInformation.heightAnchor.constraint(equalToConstant: 42),
+            
+            finalResultText.topAnchor.constraint(equalTo: textInformation.bottomAnchor, constant: 10),
+            finalResultText.centerXAnchor.constraint(equalTo: centerXAnchor),
+            finalResultText.widthAnchor.constraint(equalToConstant: 282),
+            finalResultText.heightAnchor.constraint(equalToConstant: 60 ),
+        ])
+    }
+}
+
