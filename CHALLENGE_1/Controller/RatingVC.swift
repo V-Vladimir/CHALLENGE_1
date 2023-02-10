1a08faa912f4ff39ba458fb4b814ca6e0f247239
Aleks K <alx.kan037@gmail.com>
2023-02-10 15:27:30 +0500

    Add tableView in RatingVC

--- CHALLENGE_1/Controller/RatingVC.swift
+++ CHALLENGE_1/Controller/RatingVC.swift
@@ -6,23 +6,73 @@
 //
 
 import UIKit
+import RealmSwift
 
 class RatingVC: UIViewController {
+    private let localRealm = try! Realm()
+    private var playerArray: Results<PlayerModel>! = nil
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.backgroundColor = .white
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.showsVerticalScrollIndicator = false
+        table.bounces = false
+        table.contentMode = .center
+        //table.delaysContentTouches = true
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
         setupConstraints()
+        setDelegates()
         
     }
     
     private func setupViews() {
         view.addBackground()
+        view.addSubview(tableView)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "idTestCell")
+    }
+    
+    private func setDelegates() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50)
+        
+        ])
         
     }
 
 
+}
+
+extension RatingVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "idTestCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
+
+extension RatingVC: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
 }
